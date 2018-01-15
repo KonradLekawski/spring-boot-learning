56e0572ea0c52eaebc8fc3a95b63a9b1dbf22c43
KonradLekawski <Snajperv52@gmail.com>
2018-01-15 13:58:27 +0100

    initialize folder structure

---
+++ src/main/java/com/kodekul/kondrat/bfsword/BfswordApplication.java
@@ -0,0 +1,12 @@
+package com.kodekul.kondrat.bfsword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfswordApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kodekul/kondrat/bfsword/BfswordApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodekul.kondrat.bfsword;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BfswordApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
